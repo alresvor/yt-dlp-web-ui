78e31dc1dc74b4fba35bb6aa6431aa0256bf696c
alresvor <alresvor@gmail.com>
2023-11-28 20:27:05 +0800

    Update fs.go

--- server/sys/fs.go
+++ server/sys/fs.go
@@ -6,16 +6,14 @@ import (
 
 	"github.com/marcopeocchi/yt-dlp-web-ui/server/config"
 	"github.com/marcopeocchi/yt-dlp-web-ui/server/internal"
-	"golang.org/x/sys/unix"
+	
 )
 
 // package containing fs related operation (unix only)
 
 // FreeSpace gets the available Bytes writable to download directory
 func FreeSpace() (uint64, error) {
-	var stat unix.Statfs_t
-	unix.Statfs(config.Instance().DownloadPath, &stat)
-	return (stat.Bavail * uint64(stat.Bsize)), nil
+	return 0, nil
 }
 
 // Build a directory tree started from the specified path using DFS.
